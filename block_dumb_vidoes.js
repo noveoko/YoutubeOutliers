6274939e1535f0cfed3fa63acf0576867a03f777
Marcin Kraszewski <visitwarsaw@gmail.com>
2021-06-15 19:14:52 +0200

    updated script

--- block_dumb_vidoes.js
+++ block_dumb_vidoes.js
@@ -1,20 +1,23 @@
 
-const blocked = ['reacting to','react to','win','$','insane','how to']
+const blocked = ['life as a','scammer','killer','destroyed','amazing','reacting to','react to','win','$','insane','how to','how does','why does','what does','believe in','snl']
 
 function hideVideos(blocked){
   let content = document.getElementById("contents");
   let videos = content.querySelectorAll("#content");
   videos.forEach(video=>{
+    let video_title = video.querySelector("#video-title").textContent.toLowerCase();
     let delete_me = false
     blocked.forEach(word=>{
-      if(video.querySelector("#video-title").textContent.toLowerCase().includes(word)){
+      if(video_title.includes(word)){
         delete_me = true
       }
     })
   if(delete_me==true){
   //remove video
     video.remove();
-    console.log("removed video x");
+    console.log(`removed video ${video_title}`);
   }
   })
 }
+
+hideVideos(blocked)
