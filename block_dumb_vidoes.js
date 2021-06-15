f3e1ee654eefda514a2e6ab5acde95386bfd4c78
Marcin Kraszewski <visitwarsaw@gmail.com>
2021-06-15 18:19:19 +0200

    Create block_dumb_vidoes.js

---
+++ block_dumb_vidoes.js
@@ -0,0 +1,20 @@
+
+const blocked = ['reacting to','react to','win','$','insane']
+
+function hideVideos(blocked){
+  let content = document.getElementById("contents");
+  let videos = content.querySelectorAll("#content");
+  videos.forEach(video=>{
+    let delete_me = false
+    blocked.forEach(word=>{
+      if(video[0].querySelector("#video-title").textContent.toLowerCase().includes(word)){
+        delete_me = true
+      }
+    })
+  if(delete_me==true){
+  //remove video
+    video.remove();
+    console.log("removed video x");
+  }
+  })
+}
